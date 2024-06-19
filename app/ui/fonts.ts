c392cf99107530b721d42dd83a7eaf19df1389d0
Wagner Pires Ferreira <wagner_pires@icloud.com>
2024-06-18 23:17:04 -0600

    next course chapter 3

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+
+import { Poppins, Inter, Lusitana } from 'next/font/google';
+ 
+export const poppins = Poppins({ subsets: ['latin'], weight: ['400', '700', '900'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: [ '400', '700'] });
+export const inter = Inter({ subsets: ['latin']});
